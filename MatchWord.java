8b4ae1669026fa8f1a789306c38b69cf257f8982
phage825 <phage.aria825@gmail.com>
2016-10-03 02:00:15 +0900

    Update MatchWord.java
    画面に説明文 (簡易) とテキストボックスを追加。
    しかし、まだ機能は実装されていない。

--- MatchWord.java
+++ MatchWord.java
@@ -4,27 +4,43 @@
 // import
 // ****************************************
 import javax.swing.JFrame;
+import javax.swing.JPanel;
 import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.Container;
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.Insets;
+
+
 
 // ****************************************
 // メインクラス
 // ****************************************
 class MatchWord extends JFrame {
     public static void main(String[] args) {
-        MatchWord frame = new MatchWord("Title");
+        MWindow frame = new MWindow("Title");
         frame.setVisible(true);
         
     }
-    
+}
+
+class MWindow extends JFrame {
     // ****************************************
     // コンストラクタ
     // ****************************************
-    MatchWord(String title) {
+    MWindow(String title) {
         // タイトルセット
         setTitle(title);
         
         // ウィンドウの位置とサイズ
         setBounds(100, 100, 640, 480);
+        
+        // 閉じるボタンで閉じるようにする
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
         // ****************************************
         // Icon
@@ -32,7 +48,50 @@ class MatchWord extends JFrame {
         ImageIcon icon = new ImageIcon("./MWIcon.png");
         setIconImage(icon.getImage());
         
-        // 閉じるボタンで閉じるようにする
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        // ****************************************
+        // パネルの設置
+        // ****************************************
+        // 説明文用のパネル
+        JPanel description = new JPanel();
+        
+        JLabel descriptLabel = new JLabel();
+        descriptLabel.setText("比較したい文字を入力してください。");
+        descriptLabel.setPreferredSize(new Dimension(300, 60));
+        descriptLabel.setHorizontalAlignment(JLabel.CENTER);
+        descriptLabel.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 16));
+        
+        description.add(descriptLabel);
+        
+        Container contentPane = getContentPane();
+        contentPane.add(description, BorderLayout.NORTH);
+        
+        // ****************************************
+        // テキストボックス1 (入力1) 
+        // ****************************************
+        JPanel textBox1 = new JPanel();
+        
+        JTextField text1 = new JTextField(30);
+        text1.setPreferredSize(new Dimension(400, 25));
+        text1.setFont(new Font(Font.DIALOG_INPUT, Font.PLAIN, 14));
+        text1.setHorizontalAlignment(JTextField.LEFT);
+        
+        textBox1.add(text1);
+        
+        contentPane.add(textBox1, BorderLayout.WEST);
+        
+        // ****************************************
+        // テキストボックス2 (入力2) 
+        // ****************************************
+        JPanel textBox2 = new JPanel();
+        
+        JTextField text2 = new JTextField(30);
+        text2.setPreferredSize(new Dimension(400, 25));
+        text2.setFont(new Font(Font.DIALOG_INPUT, Font.PLAIN, 14));
+        text1.setHorizontalAlignment(JTextField.LEFT);
+        
+        textBox2.add(text2);
+        
+        contentPane.add(textBox2, BorderLayout.EAST);
+        
     }
 }
